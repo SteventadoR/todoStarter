5a764950727abce61a88ea13a4177f28be149de7
Stephen Francis <schintalapudi2453@conestoga.on.ca>
2021-02-12 11:26:18 -0500

    Add today's date

--- docs/index.js
+++ docs/index.js
@@ -10,25 +10,25 @@ var $$ = Dom7;
 // Add the view
 myApp.addView('.view-main', {
 
-    // enable the dynamic navbar for this view:
-    dynamicNavbar: true
+  // enable the dynamic navbar for this view:
+  dynamicNavbar: true
 });
 
-function addTodo(sKey, sTodo){
+function addTodo(sKey, sTodo) {
   let oTodo = document.createElement("p");
   oTodo.innerHTML = `<span id="${sKey}" class="row">${sTodo}</span>`;
   document.getElementById("list").prepend(oTodo);
 }
 
 
-document.getElementById("todo").addEventListener("submit", (evt)=>{
-    evt.preventDefault();
-    const sTodo = document.getElementById("todoEntry").value;
-    if(sTodo){
-      const sId = new Date().toISOString().replace(".", "_");
-      addTodo(sId, sTodo);
-      document.getElementById("todoEntry").value = "";
-    }
+document.getElementById("todo").addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const sTodo = document.getElementById("todoEntry").value;
+  if (sTodo) {
+    const sId = new Date().toISOString().replace(".", "_");
+    addTodo(sId, sTodo);
+    document.getElementById("todoEntry").value = "";
+  }
 });
 
 window.onload = () => {
@@ -36,6 +36,6 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js');
+      .register('./sw.js');
   }
 }
